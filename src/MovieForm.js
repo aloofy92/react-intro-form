6fa2578ca88985ff60c4cba0b9a5cbfc7308945e
Brianna Hickerson <brehickerson@yahoo.com>
2023-03-09 17:32:31 -0800

    finished

---
+++ src/MovieForm.js
@@ -0,0 +1,105 @@
+import React, { Component } from 'react';
+
+
+  class MovieForm extends Component {
+
+    constructor(props){
+        super(props);
+        
+        this.initalState = {
+            title:'',
+            year: '',
+            director:'',
+            plot: '',
+            actors: [],
+            imdbRating: '',
+            dateAdded: Date()
+        };
+
+        this.state = this.initalState;
+    }
+
+
+    componentDidMount() {
+        this.timerID = setInterval(
+          () => this.tick(),
+          1000
+        );
+      }
+
+      componentWillUnmount() {
+        clearInterval(this.timerID);
+      }
+     
+      tick() {
+        this.setState({
+          dateAdded: Date()
+        });
+      }
+
+
+    handleChange = event => {
+      
+        const{name, value} = event.target;
+        this.setState ({
+            [name]: value
+        })
+    }
+
+    onFormSubmit = (event) => {
+
+        event.preventDefault();
+    
+       
+        this.props.addMovie(this.state);
+    
+
+         
+        this.setState(
+        
+            this.initalState = {
+                title:'',
+                year: '',
+                director:'',
+                plot: '',
+                actors: [],
+                imdbRating: '',
+                dateAdded: Date()
+            }
+
+            
+        );
+
+        }
+
+    
+
+    render(){
+        const { title, year, director, plot, actors, imdbRating, dateAdded } = this.state;
+        return(
+            <form onSubmit={this.onFormSubmit}>
+            <label htmlFor="title">title: </label><br/>
+            <input type="text" id="title" name="title" value={title} onChange={this.handleChange}/><br/>
+            <label htmlFor="year">year: </label><br/>
+            <input type="text" id="year" name="year" value={year} onChange={this.handleChange}/><br/>
+            <label htmlFor="director">director: </label><br/>
+            <input type="text" id="director" name="director" value={director} onChange={this.handleChange}/><br/>
+            <label htmlFor="plot">plot: </label><br/>
+            <input type="text" id="plot" name="plot" value={plot} onChange={this.handleChange}/><br/>
+            <label htmlFor="actor">actors: </label><br/>
+            <input type="text" id="actors" name="actors" value={actors} onChange={this.handleChange}/><br/>
+            <label htmlFor="imdbRating">imdbRating: </label><br/>
+            <input type="text" id="imdbRating" name="imdbRating" value={imdbRating} onChange={this.handleChange}/><br/>
+            <label htmlFor="dateAdded">dateAdded: </label><br/>
+            <input type="text" id="dateAdded" name="dateAdded" value={dateAdded} onChange={this.handleChange}/><br/>
+            <button type="submit">
+                Submit
+            </button>
+        </form> 
+        )
+    }
+
+  }
+
+
+  export default MovieForm
---
+++ src/Table.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react'
+
+const TableHead = () => {
+    return (
+      <thead>
+        <tr>
+          <th>Title</th>
+          <th>Year</th>
+          <th>Director</th>
+          <th>Plot</th>
+            <th>Actors</th>
+            <th>imdbRating</th>
+            <th>DateAdded</th>
+            <th> Delete </th>
+        </tr>
+      </thead>
+    );
+  }
+
+
+
+const TableBody = (props) => {
+
+    const rows = props.data.map((row, index) => {
+    return (      
+    <tr key ={index}>
+    <td>{row.title}</td>
+    <td>{row.year}</td>
+    <td>{row.director}</td>
+    <td>{row.plot}</td>
+    <td>{row.actors}</td>
+    <td>{row.imdbrating}</td>
+    <td>{row.dateAdded}</td>
+    <td><button onClick={() => props.removeMovie(index)}>Delete</button></td>
+    </tr>
+            )      
+    })
+    
+    return <tbody>
+      {rows}
+    </tbody>
+
+}
+
+class Table extends Component {
+    render() {
+  
+  const {movieData, removeableMovie, addMovie } = this.props;
+  
+      return (
+        <table>
+     <TableHead/>
+     <TableBody data = {movieData}  removeableMovie = {removeableMovie} addMovie = {addMovie}
+     />
+    
+        </table>
+      )
+    }
+  }
+
+export default Table
